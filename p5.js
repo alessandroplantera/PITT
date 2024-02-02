2a023f24986cb506c21d9c19e17bc0f865d9181d
alessandroplantera <156199403+alessandroplantera@users.noreply.github.com>
2024-02-02 14:47:08 +0100

    Update p5.js

--- p5.js
+++ p5.js
@@ -40,8 +40,8 @@ let isSoundPlaying = false;
 //orario sveglia//orario sveglia
 //orario sveglia
 //orario sveglia
-let alarmHo = 2;
-let alarmMi = 18;
+let alarmHo = 3;
+let alarmMi = 10;
 //orario sveglia
 //orario sveglia
 //orario sveglia//orario sveglia
